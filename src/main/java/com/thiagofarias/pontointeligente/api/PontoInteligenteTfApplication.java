853e9a6c67426e2c048596cf0a8cea606386cdca
Thiago Farias <thiagofarias99@hotmail.com>
2020-11-23 16:24:13 -0300

    Projeto criado

---
+++ src/main/java/com/thiagofarias/pontointeligente/api/PontoInteligenteTfApplication.java
@@ -0,0 +1,13 @@
+package com.thiagofarias.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteTfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteTfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagofarias/pontointeligente/api/PontoInteligenteTfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagofarias.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteTfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
